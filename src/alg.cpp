d6b1eb8780a1796befd4251177793fd69503e4cb
KekshinMaks <kekshin.maksim@gmail.com>
2026-05-30 17:42:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,15 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cctype>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-std::ifstream file(filename);
+  std::ifstream file(filename);
   if (!file) {
     std::cerr << "File error: " << filename << std::endl;
     return;
